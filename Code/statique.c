4cfbbe7a6a0594144d8eec0153d9611d3c595178
Martin Hubert <martinhub92@gmail.com>
2016-12-15 13:02:35 +0100

    ExiaSaver

--- Code/statique.c
+++ Code/statique.c
@@ -1,10 +1,5 @@
 #include "Prototypes.h"
 
-#define LARGEUR_CONSOLE 73
-
-int affichage = 0;
-
-/*
 void image ()
 {
         int i = 0, j = 0;
@@ -35,4 +30,39 @@ int main()
 }*/
 
 
+void conversion_image()
+{
+	int j;
+	int nb_colonne = NULL;
+	int nb_ligne = NULL;
+	int* Tableau = NULL;
+	FILE* pbm_j = fopen("pbm_j", "r");
 
+	fseek(pbm_j, 4, SEEK_SET);
+	nb_colonne = fgetc(pbm_j);
+	fseek(pbm_j, 6, SEEK_SET);
+	nb_ligne = fgets(Tableau, 2, pbm_j);
+
+	char** Tableau = malloc(nb_ligne * sizeof(int*));
+	for(int i = 0; i < nb_colonne; i++)
+	{
+		Tableau[i] = malloc(nb_colonne * sizeof(int));
+
+		if(Tableau == NULL)
+		{
+			exit(0);
+		}
+
+		for(i = 0; i <= nb_ligne; i++)
+		{
+			for(j = 0; j <= nb_colonne; j++)
+			{
+				fscanf("%d", &Tableau);
+
+			}
+		}
+
+	}
+
+	fclose("pbm_j");
+}
